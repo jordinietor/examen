0e51bf3028879073402918674b1b1f0ce2f0f0db
jordinietor <nietojtona@gmail.com>
2019-09-25 20:51:01 +0200

    ex2 fet

--- Projectes/EX1.java
+++ Projectes/EX1.java
@@ -1,28 +1,23 @@
-import java.util.Scanner;
-
 public class EX1 {
     public static void main(String[] args) {
-        
-        Scanner pantalla=new Scanner(System.in);
 
         String nom;
         String cognom;
         String domini;
 
         System.out.println("Introdueix el nom: ");
-        nom=pantalla.nextLine();
+        nom=System.console().readLine();
         
         System.out.println("Introdueix el cognom: ");
-        cognom=pantalla.nextLine();
+        cognom=System.console().readLine();
         
         System.out.println("Introdueix el domini: ");
-        domini=pantalla.nextLine();
+        domini=System.console().readLine();
         
 
         System.out.println("La teva direció de correu es: "+nom+"."+cognom+"@"+domini);
 
 
 
-        pantalla.close();
     }
 }
---
+++ Projectes/EX2.java
@@ -0,0 +1,25 @@
+public class EX2 {
+    public static void main(String[] args) {
+        float pes;
+        float altura;
+        float IMC;
+
+        System.out.println("Introdueix el teu pes: ");
+        pes = Float.parseFloat(System.console().readLine());
+
+        System.out.println("Introdueix el teu altura: ");
+        altura = Float.parseFloat(System.console().readLine());
+
+        IMC = pes / (altura * 2);
+        System.out.println(IMC);
+        if (IMC < 18.5) {
+            System.out.println("El teu valor de IMC es de: " + IMC + "  Estas molt prim/a");
+        } else if (IMC < 25) {
+            System.out.println("El teu valor de IMC es de: " + IMC + "  Estas bé");
+        } else if (IMC < 30) {
+            System.out.println("El teu valor de IMC es de: " + IMC + "  Sobrepes");
+        } else if (IMC > 30) {
+            System.out.println("El teu valor de IMC es de: " + IMC + "  Obesitat");
+        }
+    }
+}
