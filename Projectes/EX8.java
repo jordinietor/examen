94615a86a4d4de5814f1160c94591c4183ffbc26
jordinietor <nietojtona@gmail.com>
2019-10-01 15:44:39 +0200

    algo

---
+++ Projectes/EX8.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class EX8 {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        int numPremiat = r.nextInt(10);
+        System.out.println("El numero premiat es: " + numPremiat);
+        int arrayNums[] = new int[10];
+        for (int i = 0; i < arrayNums.length; i++)
+            arrayNums[i] = 2000;
+        boolean trobat = false;
+        int intens = 0;
+        while (!trobat) {
+            boolean coincidencia = false;
+            int numAleatori = r.nextInt(10);
+            int i = 0;
+            for (i = 0; i < arrayNums.length; i++)
+            {
+                if (numAleatori == arrayNums[i]) {
+                    coincidencia = true;
+                }
+            }
+            if (coincidencia == false) {
+                arrayNums[intens] = numAleatori;
+                intens++;
+
+                if (numAleatori == numPremiat) {
+                    trobat = true;
+
+                }
+            } else {
+                System.out.println("El numero trobat ja hi era:");
+            }
+
+        }
+        System.out.println("Premi!!!!! El numero d'intents es: " + intens);
+    }
+}
