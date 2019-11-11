a11d1668d9b2158fa8884a3bf983a65e70a747f1
jordinietor <nietojtona@gmail.com>
2019-11-11 20:34:59 +0100

    error carpetes2

--- Projectes/Pclass/Exmots.java
+++ Projectes/Pclass/Exmots.java
@@ -6,7 +6,17 @@ package Pclass;
 public class Exmots {
 
     public static void main(String[] args) {
-        
     
+    String noms[] = new String[2];
+    String nom1;
+    String nom2;
+
+    System.out.println("Introdueix el teu nom jugador1");
+
+    nom1 = System.console().readLine();
+    
+    System.out.println("Introdueix el teu nom jugador2");
+
+    nom2 = System.console().readLine();
     }
 }
